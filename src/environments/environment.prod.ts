37e0b059a6f20b6b6474d1ab799b48d7f4182df0
Vadim Boyko <vadim.boyko.01@outlook.com>
2020-06-07 19:08:30 +0300

    admin login created

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyCWvbakghZHGaW5i-0dyLly1kynUI9ah90'
 };
---
+++ src/environments/interface.ts
@@ -0,0 +1,10 @@
+export interface Environment {
+  apiKey: string,
+  production: boolean,
+  returnSecureToken?: boolean
+}
+
+export interface FbAuthResponse {
+  idToken: string
+  expiresIn: string
+}
